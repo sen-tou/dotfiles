c4a2d8fc4e0009d5c9d58f79f050323a64f62591
sen-tou <29461308+sen-tou@users.noreply.github.com>
2025-01-12 21:45:39 +0100

    change some wezterm appearance

--- wezterm/.config/wezterm/lua/appearance.lua
+++ wezterm/.config/wezterm/lua/appearance.lua
@@ -8,21 +8,10 @@ M.apply_to_config = function(c)
     c.window_background_opacity = 0.86
     c.inactive_pane_hsb = { brightness = 0.8 }
     c.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-    c.show_new_tab_button_in_tab_bar = true
+    c.enable_tab_bar = false
     c.use_fancy_tab_bar = false
-    c.colors = {
-        tab_bar = {
-            background = scheme.background,  -- Set the desired background color for the tab bar
-            active_tab = {
-                bg_color = scheme.brights[6],  -- Background color for inactive tabs
-                fg_color = scheme.background,  -- Foreground color for inactive tab text
-            },
-            inactive_tab = {
-                bg_color = scheme.background,
-                fg_color = scheme.foreground,
-            },
-        },
-    }
+    c.colors = {}
+    c.window_background_opacity = 0.8
 end
 
 return M
